ece15835536d7e4b9d12c1a85a9b83329495ea79
ethanm32 <c19305451@mytudublin.ie>
2022-11-14 10:25:24 +0000

    problem1-part1
    Solving on click 1 with streams

--- problem1.js
+++ problem1.js
@@ -1,52 +1,65 @@
 let counter = 0;
-function createNote() {
-    counter++;
-    let divnote = document.createElement('div');
-    var text = document.getElementById("text-note").value;
-    divnote.innerHTML += text;
-    divnote.setAttribute("id", "div" + counter);
-    document.body.appendChild(divnote);
-    var deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute("id", "div" + counter);
-    deleteBtn.innerHTML = "Delete";
-    var editBtn = document.createElement('button');
-    editBtn.innerHTML = "Edit";
-    editBtn.setAttribute("id", "div" + counter);
-    document.body.appendChild(deleteBtn);
-    divnote.style.width = "fit-content";
-    document.body.appendChild(editBtn);
-    divnote.style.whiteSpace = "pre-wrap";
-    divnote.style.wordWrap = "word-break";
-    var backgcol = document.getElementById('colour').value;
-    
+var addBtn = document.getElementById("addBtn");
 
-    divnote.style.backgroundColor = backgcol;
-    divnote.style.border = "1px solid black";
-    divnote.style.borderRadius = "5px";
 
-    deleteBtn.onclick = function () {
-        var id = event.srcElement.id;
-        document.getElementById(id).innerHTML = "";
-        deleteBtn.remove();
-        editBtn.remove();
-    };
+const observeAdd = {
+    next: function() {
+        counter++;
+        let divnote = document.createElement('div');
+        var text = document.getElementById("text-note").value;
+        divnote.innerHTML += text;
+        divnote.setAttribute("id", "div" + counter);
+        document.body.appendChild(divnote);
+        var deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute("id", "div" + counter);
+        deleteBtn.innerHTML = "Delete";
+        var editBtn = document.createElement('button');
+        editBtn.innerHTML = "Edit";
+        editBtn.setAttribute("id", "div" + counter);
+        document.body.appendChild(deleteBtn);
+        divnote.style.width = "fit-content";
+        document.body.appendChild(editBtn);
+        divnote.style.whiteSpace = "pre-wrap";
+        divnote.style.wordWrap = "word-break";
+        var backgcol = document.getElementById('colour').value;
 
-    editBtn.onclick = function() {
-        var id = event.srcElement.id;
-        document.getElementById(id).contentEditable = "true"; 
-        var saveBtn = document.createElement('button');
-        saveBtn.innerHTML = "Save Changes";
-        saveBtn.setAttribute("id", id);
-        
-        document.body.appendChild(saveBtn);
-        
-        saveBtn.onclick = function() {
-            var saveId = event.srcElement.id; 
-            console.log(counter);
-            document.getElementById(saveId).contentEditable = "false";
-            saveBtn.remove();
-        }
-    }   
 
-    return divnote.id;
+        divnote.style.backgroundColor = backgcol;
+        divnote.style.border = "1px solid black";
+        divnote.style.borderRadius = "5px";
+
+        //deleteBtn.onclick = function () {
+        //    var id = event.srcElement.id;
+        //    document.getElementById(id).innerHTML = "";
+        //    deleteBtn.remove();
+        //    editBtn.remove();
+        //};
+
+        var observable = Rx.Observable.fromEvent(deleteBtn, 'click');
+
+        observable.subscribe(x => console.log(x));
+
+        editBtn.onclick = function() {
+            var id = event.srcElement.id;
+            document.getElementById(id).contentEditable = "true"; 
+            var saveBtn = document.createElement('button');
+            saveBtn.innerHTML = "Save Changes";
+            saveBtn.setAttribute("id", id);
+            
+            document.body.appendChild(saveBtn);
+            
+            saveBtn.onclick = function() {
+                var saveId = event.srcElement.id; 
+                console.log(counter);
+                document.getElementById(saveId).contentEditable = "false";
+                saveBtn.remove();
+            }
+        }  
+        return divnote.id;
+    }
 }
+
+var addObservable = Rx.Observable.fromEvent(addBtn, 'click');
+addObservable.subscribe(observeAdd);
+
+    
